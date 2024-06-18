d44e5a3a174cc2549bbf1b2b662a558e37a0f41f
ashish <ashishvarma1729@gmail.com>
2024-06-19 00:30:50 +0530

    env vars modified

--- client/src/components/Layouts/footer.js
+++ client/src/components/Layouts/footer.js
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="bg-dark text-light p-3">
-      <h6 className="text-center">All Rights Reserved &copy; Vinay</h6>
+      <h6 className="text-center">All Rights Reserved &copy; Ashish</h6>
     </div>
   );
 };
